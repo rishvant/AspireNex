2f95f696808a6900be8469f68e4d4a70fc9f4808
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-07-10 19:58:09 +0530

    add: api url

--- Task-2/client/src/utils/quizService.js
+++ Task-2/client/src/utils/quizService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/quizzes';
+const API_URL = 'https://aspirenex-4oaq.onrender.com/api/quizzes';
 
 export const getAllQuizzes = async () => {
   const response = await axios.get(API_URL);
